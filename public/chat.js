eaf320edbb28ae32703ab8a66c5f44ff789ac57a
Vardaan Aashish <vardaan119@gmail.com>
2019-05-30 22:39:34 -0400

    [A] button event listener

--- public/chat.js
+++ public/chat.js
@@ -6,3 +6,12 @@ const message = document.getElementById('message');
 const handle = document.getElementById('handle');
 const btn = document.getElementById('send');
 const output = document.getElementById('output');
+
+// emit event
+// when send button clicked, emit event via websocket with values of inputs
+btn.addEventListener('click', () => {
+  socket.emit('chat', {
+    message: message.value,
+    handle: handle.value
+  });
+});
